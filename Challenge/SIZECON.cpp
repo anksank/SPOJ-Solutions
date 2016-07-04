6fd388b4a6c111984a913249c8c644f7214d6585
Ankit <ankit@sap.com>
2016-07-04 19:31:31 +0530

    adding Challenge problems as well

---
+++ Challenge/SIZECON.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    int sum = 0,n;
+    while(t--)
+    {
+        cin>>n;
+        if(n<0);
+        else sum+=n;
+    }
+    cout<<sum<<"\n";
+    return 0;
+}
